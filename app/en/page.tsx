e7ce23927f9eae8d276e816916c9ceee377030e0
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2023-08-25 00:50:28 +0330

    First commit to push

--- app/en/page.tsx
+++ app/en/page.tsx
@@ -2,15 +2,10 @@ import Nav from "@/components/main page/Nav";
 import About from "@/components/main page/About";
 import Menu from "@/components/main page/menu";
 import Fotter from "@/components/main page/Fotter";
-import { mode, setMode } from "@/components/main page/changeMode";
 export default function page() {
   return (
-    <div
-      className={`${
-        mode === "Dark" ? "dark:bg-stone-950 dark:text-white" : ""
-      } lalezar`}
-    >
-      <Nav setMode={setMode} Lang="En" />
+    <div className="bg-white text-black dark:bg-stone-950 dark:text-white ">
+      <Nav Lang="En" />
       <About Lang="En" />
       <Menu Lang="En" />
       <Fotter Lang="En" />

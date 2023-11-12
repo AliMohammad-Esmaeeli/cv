491e6aaad3ec03e42e6621c4e68ce751da7076d5
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2023-11-12 14:59:14 +0330

    Add Goftino chat

--- app/en/page.tsx
+++ app/en/page.tsx
@@ -1,4 +1,5 @@
 import Nav from "@/components/main page/Nav";
+import Chat from "@/components/main page/Chat";
 import About from "@/components/main page/About";
 import Menu from "@/components/main page/menu";
 import Fotter from "@/components/main page/Fotter";
@@ -6,6 +7,7 @@ export default function page() {
   return (
     <div className="bg-white text-black dark:bg-stone-950 dark:text-white overflow-x-hidden">
       <Nav Lang="En" />
+      <Chat />
       <About Lang="En" />
       <Menu Lang="En" />
       <Fotter Lang="En" />
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Nav from "@/components/main page/Nav";
+import Chat from "@/components/main page/Chat";
 import About from "@/components/main page/About";
 import Menu from "@/components/main page/menu";
 import Fotter from "@/components/main page/Fotter";
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <div className="bg-white text-black dark:bg-stone-950 dark:text-white overflow-x-hidden">
       <Nav Lang="Fa" />
+      <Chat />
       <About Lang="Fa" />
       <Menu Lang="Fa" />
       <Fotter Lang="Fa" />

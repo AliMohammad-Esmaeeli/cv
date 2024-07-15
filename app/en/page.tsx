8376935973fa125cb196380069bee468f14aea6f
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2024-07-15 19:11:37 +0330

    fix: seprate components to increase website speed

--- app/en/page.tsx
+++ app/en/page.tsx
@@ -1,5 +1,6 @@
 import Nav from "@/components/HomePage/Nav";
 import Chat from "@/components/HomePage/Chat";
+import Main from "@/components/HomePage/Main";
 import About from "@/components/HomePage/About";
 import Skills from "@/components/HomePage/Skills";
 import Fotter from "@/components/HomePage/Fotter";
@@ -11,6 +12,7 @@ export default function page() {
     <div className="min-h-dvh scroll-smooth overflow-x-hidden select-none appearance-none bg-white text-black dark:bg-stone-950 dark:text-white">
       <Nav Lang="En" />
       <Chat />
+      <Main Lang="En" />
       <About Lang="En" />
       <Suspense fallback={<Loading />}>
         <Skills Lang="En" />
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Nav from "@/components/HomePage/Nav";
 import Chat from "@/components/HomePage/Chat";
+import Main from "@/components/HomePage/Main";
 import About from "@/components/HomePage/About";
 import Skills from "@/components/HomePage/Skills";
 import Fotter from "@/components/HomePage/Fotter";
@@ -11,6 +12,7 @@ export default function Home() {
     <div className="min-h-dvh scroll-smooth overflow-x-hidden select-none appearance-none bg-white text-black dark:bg-stone-950 dark:text-white font-yekanBakh">
       <Nav Lang="Fa" />
       <Chat />
+      <Main Lang="Fa" />
       <About Lang="Fa" />
       <Suspense fallback={<Loading />}>
         <Skills Lang="Fa" />

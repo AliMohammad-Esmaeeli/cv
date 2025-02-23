ac845a2a4841cadff924abfd7bbc14c86231e777
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2025-02-23 22:40:34 +0330

    refactor: change title font

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -12,11 +12,9 @@ export default function BlogIndex() {
     <div className="max-w-4xl mx-auto px-4 py-12 min-h-dvh scroll-smooth overflow-x-hidden select-none appearance-none font-yekanBakh bg-white text-black dark:bg-stone-950 dark:text-white">
       <header className='flex justify-center items-center flex-col'>
         <h1 className="text-4xl font-bold mb-8">وبلاگ</h1>
-        <span className='flex justify-center items-center gap-2'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
-          </svg>
-          <p className="text-lg">This is my persion weblog🖐😁</p>
+        <span className='flex justify-center items-center gap-1'>
+          <p className="text-lg font-sans">This is my persion weblog</p>
+          <p className="font-iosEmoji">🖐😁</p>
         </span>
       </header>
 

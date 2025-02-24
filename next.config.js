65c291ba21d7ed13c4d1554c23757c0f57c30452
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2025-02-24 20:11:28 +0330

    feat: redirect /Links to /contact

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/links",
+        destination: "/contact",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

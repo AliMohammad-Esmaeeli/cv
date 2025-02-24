a83be540f8a05aa978929c5a884d9cd25c57ebdc
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2025-02-24 11:47:34 +0330

    fix: fix responsive bug

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -16,14 +16,14 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
   return (
     <article className="max-w-3xl mx-auto px-4 py-12 min-h-dvh scroll-smooth overflow-x-hidden select-none appearance-none font-yekanBakh bg-white text-black dark:bg-stone-950 dark:text-white flex-center flex-col">
       <header className="relative w-full">
-        <span className='flex flex-col justify-center items-end rounded-[0.875rem] p-10 py-11 bg-white bg-opacity-20 backdrop-blur-lg absolute bottom-5 right-4 h-10 w-[40%]'>
-          <h1 className="text-lg font-bold mb-2 text-white">{post.title}</h1>
-          <time className="text-muted-foreground text-white text-base">
+        <span className='flex flex-col justify-center items-end rounded-[0.875rem] px-8 py-11 bg-white bg-opacity-20 backdrop-blur-lg absolute bottom-5 right-4 h-10 w-[50%] truncate overflow-hidden'>
+          <h1 className="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-bold mb-2 text-white">{post.title}</h1>
+          <time className="text-muted-foreground text-white text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">
             {format(new Date(post.date), 'MMMM d, yyyy')}
           </time>
         </span>
         <Image
-          className='rounded-[1.875rem] w-full h-96 '
+          className='rounded-[1.875rem] w-full h-auto'
           src={post.image}
           alt={post.title}
           height={1000}

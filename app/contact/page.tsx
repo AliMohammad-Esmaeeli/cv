aceee67401e562f4d85b1077f778c389773b15d8
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2025-02-24 20:10:33 +0330

    feat: Add contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Link from 'next/link';
+import socialLinks from '@/Data/socialLinks.json';
+import { Fragment } from 'react';
+
+export default function Page() {
+    return (
+        <div className="min-h-dvh w-screen scroll-smooth overflow-x-hidden select-none appearance-none bg-[#faf3ea] text-black font-yekanBakh flex-center flex-col relative">
+            <div className="grid grid-cols-3 md:grid-cols-5 gap-x-6 gap-y-20 justify-center items-center w-1/2 h-full">
+                {socialLinks.socialLinks.map((link: any, index: any) => (
+                    <Fragment key={link.name}>
+                        <a href={link.url} target="_blank" rel="noopener noreferrer" className="group relative flex flex-col items-center">
+                            <i className={link.icon} aria-hidden="true">
+                                <img
+                                    alt={link.name}
+                                    className="block group-hover:hidden size-10"
+                                    src={`/images/svgs/${link.name}.svg`}
+                                />
+                                <img
+                                    alt={link.name}
+                                    className="hidden group-hover:block size-10"
+                                    src={`/images/svgs/${link.name}-colorfull.svg`}
+                                />
+                                <span className="hidden group-hover:block text-sm text-white bg-black px-3 py-2 rounded-md absolute -translate-x-2 -translate-y-24  after:rotate-45 after:absolute after:!bg-black after:size-4 after:rounded-sm after:top-6 after:right-6 after:-z-50">{link.persionName}</span>
+                            </i>
+                        </a>
+                    </Fragment>
+                ))}
+                <div className="size-16 bg-purple-400 blur-xl ring-purple-400/50 shadow-2xl shadow-purple-400/5 rounded-full absolute"></div>
+            </div>
+        </div>
+    );
+}

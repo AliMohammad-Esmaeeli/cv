b77c0b9124bc118df39db79732a03cc3942acc87
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2025-02-24 23:25:14 +0330

    fix: fix responsive bug and breadcrumbs color

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,7 +9,7 @@ export default function BlogIndex() {
   const posts = getSortedPostsData();
 
   return (
-    <div className="max-w-4xl mx-auto px-4 py-12 min-h-dvh scroll-smooth overflow-x-hidden select-none appearance-none font-yekanBakh bg-white text-black dark:bg-stone-950 dark:text-white">
+    <div className="max-w-3xl px-3 py-12 min-h-dvh scroll-smooth overflow-x-hidden select-none appearance-none font-yekanBakh bg-white text-black dark:bg-stone-950 dark:text-white">
       <header className='flex-center flex-col'>
         <h1 className="text-4xl font-bold mb-8">وبلاگ</h1>
         <span className='flex-center gap-1'>

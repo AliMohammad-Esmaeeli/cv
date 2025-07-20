a5f1c79b5f9654826b9436780cfbd44679868e33
AliMohammad-Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2025-07-20 15:12:13 +0330

    refactor: Transferring files to src folder

---
+++ src/components/blog/ShareButton.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { Button } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
+import { useLanguage } from "../../locales/locales";
+
+interface PropsType {
+  title: string;
+}
+
+export default function ShareButton(props: PropsType) {
+  const { translations } = useLanguage();
+  const pathname = usePathname();
+  return (
+    <Button
+      className="w-full h-14 mt-6 bg-[#009f4d] py-2 px-4 rounded-md hover:bg-[#00793a] transition-colors flex-center gap-2 flex-row-reverse"
+      onClick={() => {
+        navigator.share({
+          url: `${location.origin + pathname}`,
+          title: props.title,
+        });
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6 fill-white stroke-white"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
+        />
+      </svg>
+      <p className="text-lg text-white">{translations.blog.share}</p>
+    </Button>
+  );
+}
